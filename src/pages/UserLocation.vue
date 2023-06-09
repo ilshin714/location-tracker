bb33b9626e5addbe9af30723cc343209e609f73f
Ilshin Ji <ilshin714@gmail.com>
2023-06-09 18:02:34 -0400

    Created Draft Input UI

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Start</h1>
+  <section class="ui two column centered grid">
+    <div class="column">
+        <form class="ui segment large form">
+            <div class="ui message red"></div>
+            <div class="ui segment">
+                <div class="field ui grid">
+                    <div class="thirteen wide column">
+                        <div class= "ui right icon input right">
+                            <input type="text" placeholder="Please enter your location">
+                            <i class="icon map marker alternate teal"/>
+                        </div>
+                    </div>
+                    <div class="three wide column">
+                        <button class="ui button large teal">SEARCH</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+  </section>
 </template>
+
+<script>
+
+</script>
